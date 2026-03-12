2d3d51cf2081e94043094363a262ee0dc07de69c
prathyoush24 <prathyoush2004@gmail.com>
2026-03-12 22:20:41 +0530

    11

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login"; // import login page
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/homepage" element={<HomePage />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
