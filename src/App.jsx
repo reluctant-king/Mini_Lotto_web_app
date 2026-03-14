111d2e6ffd48ccae4970953d59634a0eb25b2ccd
prathyoush24 <prathyoush2004@gmail.com>
2026-03-14 13:45:49 +0530

    11

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
-import Otp from "./pages/Otp";
-=======
 import HomePage from "./components/Home/HomePage";
 import Login from "./components/Login/Login";
 import Otp from "./components/Login/Otp";
@@ -12,17 +7,13 @@ import BottomNav from "./components/Footer/BottomNav";
 import RechargeTable from "./components/Recharge/RechargeTable";
 import TicketSlider from "./components/Tickets/TicketSlider";
 import WinnerSlider from "./components/Winner Slider/WinnerSlider";
->>>>>>> main
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
 
-<<<<<<< HEAD
-=======
       <Header />
->>>>>>> main
       <Toaster position="top-center" />
 
       <Routes>
@@ -30,10 +21,6 @@ function App() {
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/otp" element={<Otp />} />
-<<<<<<< HEAD
-      </Routes>
-
-=======
         <Route path="/recharge" element={<RechargeTable />} />
         <Route path="/tickets" element={<TicketSlider />} />
         <Route path="/winners" element={<WinnerSlider />} />
@@ -41,7 +28,6 @@ function App() {
 
       <BottomNav />
 
->>>>>>> main
     </BrowserRouter>
   );
 }
