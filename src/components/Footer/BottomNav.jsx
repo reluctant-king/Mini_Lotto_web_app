b0c4c87690e8847492c4e5eef6ce1233ce3f16a8
prathyoush24 <prathyoush2004@gmail.com>
2026-03-14 14:06:02 +0530

    11

--- src/components/Footer/BottomNav.jsx
+++ src/components/Footer/BottomNav.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./bottomNav.css";
 
-import { FaDice, FaTicketAlt, FaTrophy, FaUser } from "react-icons/fa";
+import { FaHome, FaDice, FaTicketAlt, FaUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 export default function BottomNav() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="bottom-nav">
 
-      <div className="nav-item">
+      <div className="nav-item" onClick={() => navigate("/homepage")}>
+        <FaHome />
+        <span>Home</span>
+      </div>
+
+      <div className="nav-item" onClick={() => navigate("/games")}>
         <FaDice />
         <span>Games</span>
       </div>
 
-      <div className="nav-item">
+      <div className="nav-item" onClick={() => navigate("/tickets")}>
         <FaTicketAlt />
         <span>My Tickets</span>
       </div>
 
-      <div className="nav-item">
+      <div className="nav-item" onClick={() => navigate("/account")}>
         <FaUser />
         <span>Account</span>
       </div>
