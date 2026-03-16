2c78bcd04a5f83b17d1dceb94f036b1688aba190
prathyoush24 <prathyoush2004@gmail.com>
2026-03-16 16:10:38 +0530

    call button function update

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./account.css";
 
 export default function Account() {
+  const agentPhone = "+919876543210";
+
   return (
     <div className="ap">
       {/* HERO */}
@@ -95,11 +97,11 @@ export default function Account() {
               <div className="agent-title">+91 98765 43210</div>
             </div>
             <div className="agent-right">
-              <button className="call-btn" title="Call agent">
+              <a href={`tel:${agentPhone}`} className="call-btn" title="Call agent">
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.27h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.91a16 16 0 0 0 6 6l.92-.92a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" />
                 </svg>
-              </button>
+              </a>
             </div>
           </div>
         </div>
